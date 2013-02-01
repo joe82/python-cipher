d3fc42e4c6862a2ed5faf5565f380cec128b5467
schnabear <schnabear@gmail.com>
2013-02-01 13:15:04 +0800

    Added mode for enciphering and deciphering.

--- rot13.py
+++ rot13.py
@@ -2,8 +2,10 @@
 ROT13 Function
 
 string = string to be (en/de)ciphered
+mode   = accepted values are 1 (encipher) and -1 (decipher)
+       = multiplied with the key
 
 http://ja.wikipedia.org/wiki/ROT13
 """
-def rot13(string):
-    return ''.join([[s, chr((((ord(s)-[97, 65][s.isupper()])+13)%26)+[97, 65][s.isupper()])][s.isalpha()] for s in string])
+def rot13(string, mode = 1):
+    return ''.join([[s, chr((((ord(s)-[97, 65][s.isupper()])+13*mode)%26)+[97, 65][s.isupper()])][s.isalpha()] for s in string])
